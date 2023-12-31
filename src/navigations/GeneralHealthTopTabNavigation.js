f638eab147709646038d8fff4efc7740e4fb5413
Da-Yung <dayungbe@yahoo.com>
2023-12-31 18:15:53 -0500

    Adding top tab menus

--- src/navigations/GeneralHealthTopTabNavigation.js
+++ src/navigations/GeneralHealthTopTabNavigation.js
@@ -1,11 +1,37 @@
 import { StyleSheet, Text, View } from "react-native";
-import NavBar from "../components/controls/NavBar";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import NavBar from "../components/NavBar";
+import GeneralHealth from "../screens/generalHealth/GeneralHealth";
+import BloodPressure from "../screens/generalHealth/BloodPressure";
+import Step from "../screens/generalHealth/Step";
+import Weight from "../screens/generalHealth/Weight";
 
 const GeneralHealthTopTabNavigation = () => {
+   const tab = createMaterialTopTabNavigator();
    return (
-      <View>
-         <NavBar />
-         <Text>GeneralHealthTopTabNavigation</Text>
+      <View style={{ flex: 1 }}>
+         <NavBar
+            title="General Health"
+            backScreen="Home"
+         />
+         <tab.Navigator initialRouteName="generalHealth">
+            <tab.Screen
+               name="generalHealth"
+               component={GeneralHealth}
+            />
+            <tab.Screen
+               name="weight"
+               component={Weight}
+            />
+            <tab.Screen
+               name="bloodPressure"
+               component={BloodPressure}
+            />
+            <tab.Screen
+               name="step"
+               component={Step}
+            />
+         </tab.Navigator>
       </View>
    );
 };
--- src/navigations/PersonalInformationStackNavigation.js
+++ src/navigations/PersonalInformationStackNavigation.js
@@ -1,11 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import NavBar from "../components/controls/NavBar";
+import PersonalInformation from "../screens/personalInformation/PersonalInformation";
+import InsuranceInformation from "../screens/personalInformation/InsuranceInformation";
 
 const PersonalInformationStackNavigation = () => {
+   const tab = createMaterialTopTabNavigator();
    return (
-      <View>
-         <NavBar />
-         <Text>PersonalInformationStackNavigation</Text>
+      <View style={{ flex: 1 }}>
+         <NavBar
+            title="Information"
+            backScreen="Home"
+         />
+         <tab.Navigator initialRouteName="personalInformation">
+            <tab.Screen
+               name="personal"
+               component={PersonalInformation}
+            />
+            <tab.Screen
+               name="insurance"
+               component={InsuranceInformation}
+            />
+         </tab.Navigator>
       </View>
    );
 };
